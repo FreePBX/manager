39bf510ef5dfe5c1a04ce7940b3d67722243c888
Diego Iastrubni <diego.iastrubni@xorcom.com>
2007-06-27 15:17:48 +0000

    fix for ticket:1776 - add sqlite support for the manager module (freepbx 2.3 only)

---
+++ install.php
@@ -0,0 +1,23 @@
+<?php
+
+global $db;
+global $amp_conf;
+
+$autoincrement = (($amp_conf["AMPDBENGINE"] == "sqlite") || ($amp_conf["AMPDBENGINE"] == "sqlite3")) ? "AUTOINCREMENT":"AUTO_INCREMENT";
+
+$sql = "CREATE TABLE IF NOT EXISTS manager (
+	`manager_id` INTEGER NOT NULL PRIMARY KEY $autoincrement,
+	`name` VARCHAR( 15 ) NOT NULL ,
+	`secret` VARCHAR( 50 ) ,
+	`deny` VARCHAR( 255 ) ,
+	`permit` VARCHAR( 255 ) ,
+	`read` VARCHAR( 50 ) ,
+	`write` VARCHAR( 50 )
+)";
+
+$check = $db->query($sql);
+if (DB::IsError($check)) {
+	die("Can not create `manager` table" .  $check->getMessage() .  "\n");
+}
+
+?>
--- uninstall.sql
+++ uninstall.sql
@@ -1,2 +1,2 @@
 
-DROP TABLE IF EXISTS manager;
+DROP TABLE IF EXISTS `manager`;
