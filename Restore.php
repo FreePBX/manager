fcb5040a06ddcf82368849b86275c6cccf25de19
kapil gupta <kgupta@sangoma.com>
2019-09-13 11:34:59 +0530

    fixing typo error

--- Restore.php
+++ Restore.php
@@ -2,7 +2,7 @@
 namespace FreePBX\modules\Manager;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
-	public function runRestore){
+	public function runRestore () {
 			$configs = $this->getConfigs();
 			foreach ($configs as $manager) {
 				$this->FreePBX->Manager->upsert((int)$manager['manager_id'], $manager['name'], $manager['secret'], $manager['deny'], $manager['permit'], $manager['read'], $manager['write'], $manager['writetimeout']);
