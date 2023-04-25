d3226c7df2aa895227cc6f5e0f9210b814043a4f
Javier Pastor <jpastor@cerebelum.net>
2023-04-25 07:22:19 +0200

    open wiki in new window.

--- views/page.main.php
+++ views/page.main.php
@@ -6,7 +6,7 @@
 		<div class="alert alert-info">
 			<?php
 				echo "<p>"._("The default Asterisk Manager Interface (AMI) configuration has changed starting in version 16. ");
-				echo sprintf( _('Please refer to  <a href="%s"> <b>wiki</b> </a> for more information.'), "https://wiki.sangoma.com/display/FPG/AMI+Default+Configuration+in+16")."</p>";
+				echo sprintf( _('Please refer to  <a href="%s" target="_blank"> <b>wiki</b> </a> for more information.'), "https://wiki.sangoma.com/display/FPG/AMI+Default+Configuration+in+16")."</p>";
 				echo "<p>".sprintf( _('AMI current settings for Bind Address : %s and bind port : %s.'), $manager->readConfig('bindaddr'), $manager->readConfig('port'))."</p>";
 			 ?>
 		</div>
